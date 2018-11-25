d90ac22043e68f15bbd770aa00a3c8f57d210863
Xudong He <hex3456@gmail.com>
2018-11-25 15:54:24 -0600

    Create 225. Implement Stack using Queues.java

---
+++ Design/225. Implement Stack using Queues.java
@@ -0,0 +1,67 @@
+225. Implement Stack using Queue
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+Example:
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+
+using node 
+class MinStack {
+    private Node head;
+    
+    /** initialize your data structure here. */
+    public MinStack() {
+        this.head = null;
+    }
+    
+    public void push(int x) {
+        if(head == null)
+            head = new Node(x,x,null);
+        else
+            head = new Node(x,Math.min(x,head.min),head);
+    }
+    
+    public void pop() {
+        head = head.next;
+    }
+    
+    public int top() {
+        return head.val;
+    }
+    
+    public int getMin() {
+        return head.min;
+    }
+    
+    private class Node{
+        int val;
+        int min;
+        Node next;
+        
+        private Node(int val, int min,Node next){
+            this.val = val;
+            this.min = min;
+            this.next = next;
+        }
+    }
+}
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
