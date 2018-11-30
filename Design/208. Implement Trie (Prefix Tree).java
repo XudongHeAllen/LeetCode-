1cb831c71e05893fdd6cd141e121a8e8b55f04e9
Xudong He <hex3456@gmail.com>
2018-11-30 12:27:19 -0600

    Update 208. Implement Trie (Prefix Tree).java

--- Design/208. Implement Trie (Prefix Tree).java
+++ Design/208. Implement Trie (Prefix Tree).java
@@ -90,3 +90,59 @@ class Trie {
  * boolean param_2 = obj.search(word);
  * boolean param_3 = obj.startsWith(prefix);
  */
+
+
+class TrieNode{
+    TrieNode[] neighbors;
+    boolean isEnd;
+    
+    public TrieNode(){
+        neighbors = new TrieNode[26];
+        isEnd = false;
+    }
+}
+class Trie {
+    private TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode cur = root;
+        for(char c: word.toCharArray()){
+            if(cur.neighbors[c-'a']==null){
+                cur.neighbors[c-'a'] = new TrieNode();
+            }
+            cur = cur.neighbors[c-'a'];
+        }
+        cur.isEnd = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode cur = root;
+        for(char c: word.toCharArray()){
+            if(cur.neighbors[c-'a']!=null){
+                cur = cur.neighbors[c-'a'];
+            } else {
+                return false;
+            }
+        }
+        return cur.isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode cur = root;
+        for(char c: prefix.toCharArray()){
+            if(cur.neighbors[c-'a']!=null){
+                cur = cur.neighbors[c-'a'];
+            } else {
+                return false;
+            }
+        }
+        return true;
+    }
+}
