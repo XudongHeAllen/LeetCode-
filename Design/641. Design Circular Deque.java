74fdf96c607798803484316758990c1f986f472a
Xudong He <hex3456@gmail.com>
2018-11-30 23:13:39 -0600

    Update 641. Design Circular Deque.java

--- Design/641. Design Circular Deque.java
+++ Design/641. Design Circular Deque.java
@@ -12,76 +12,74 @@ getRear(): Gets the last item from Deque. If the deque is empty, return -1.
 isEmpty(): Checks whether Deque is empty or not. 
 isFull(): Checks whether Deque is full or not.
 
-	private class NodeDouble{
-		int val;
-		NodeDouble next;
-		NodeDouble prev;
-		
-		public NodeDouble(int val){
-			this.val = val;
-			this.next = null;
-			this.prev = null;
-		}
-		
-	}
-	
-	NodeDouble front; 
-	NodeDouble rear; 
-	int max , size;
-	
+class MyCircularQueue {
+
     /** Initialize your data structure here. Set the size of the queue to be k. */
-    public Design_Circular_Queue_2(int k) {
-        this.max = k;
-        this.size = 0;
-        this.front = new NodeDouble(0);
-        this.rear = new NodeDouble(0);
-        this.front.next = this.rear;
-        this.rear.prev = this.front;
+    int append, pop, size, k;
+    int[] q;
+    public MyCircularQueue(int k) {
+        append = 0;
+        pop = -1;
+        size = 0;
+        this.k = k;
+        q = new int[k];
     }
     
     /** Insert an element into the circular queue. Return true if the operation is successful. */
-    public boolean enQueue(int val) {
-        if(!isFull()){
-        	size++;
-        	NodeDouble n = new NodeDouble(val);
-        	n.prev = this.rear.prev;
-        	this.rear.prev.next = n;
-        	n.next = this.rear;
-        	this.rear.prev = n;
-        	return true;
+    public boolean enQueue(int value) {
+        if (size == k) {
+            return false;
         }
-    	
-        return false;
+        append %= k;
+        q[append] = value;
+        append++;
+        size++;
+        return true;
     }
     
     /** Delete an element from the circular queue. Return true if the operation is successful. */
     public boolean deQueue() {
-        if(!isEmpty()){
-        	size--;
-        	this.front.next = this.front.next.next;
-        	this.front.next.prev = this.front;
-        	return true;
+        if (size == 0) {
+            return false;
         }
-        
-        return false;
+        pop++;
+        pop %= k;
+        size--;
+        return true;
     }
     
     /** Get the front item from the queue. */
     public int Front() {
-        return isEmpty() ? -1 : this.front.next.val;
+        if (size == 0) return -1;
+        if (pop == k - 1)
+            return q[0];
+        return q[pop + 1];
     }
     
     /** Get the last item from the queue. */
     public int Rear() {
-    	return isEmpty() ? -1 : this.rear.prev.val;
+        if (size == 0) return -1;
+        return q[append - 1];
     }
     
     /** Checks whether the circular queue is empty or not. */
     public boolean isEmpty() {
-        return (this.size == 0);
+        return size == 0;
     }
     
     /** Checks whether the circular queue is full or not. */
     public boolean isFull() {
-        return (this.size == this.max);
+        return size == k;
     }
+}
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue obj = new MyCircularQueue(k);
+ * boolean param_1 = obj.enQueue(value);
+ * boolean param_2 = obj.deQueue();
+ * int param_3 = obj.Front();
+ * int param_4 = obj.Rear();
+ * boolean param_5 = obj.isEmpty();
+ * boolean param_6 = obj.isFull();
+ */
