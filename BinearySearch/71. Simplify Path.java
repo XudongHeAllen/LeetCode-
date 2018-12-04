8677ed3321097f8ac2f87be6377950d483d77b07
Xudong He <hex3456@gmail.com>
2018-12-04 00:11:18 -0600

    Create 71. Simplify Path.java

---
+++ BinearySearch/71. Simplify Path.java
@@ -0,0 +1,41 @@
+Given an absolute path for a file (Unix-style), simplify it. 
+
+For example,
+path = "/home/", => "/home"
+path = "/a/./b/../../c/", => "/c"
+path = "/a/../../b/../c//.//", => "/c"
+path = "/a//b////c/d//././/..", => "/a/b/c"
+
+In a UNIX-style file system, a period ('.') refers to the current directory, so it can be ignored in a simplified path. Additionally, a double period ("..") moves up a directory, so it cancels out whatever the last directory was. For more information, look here: https://en.wikipedia.org/wiki/Path_(computing)#Unix_style
+
+Corner Cases:
+
+Did you consider the case where path = "/../"?
+In this case, you should return "/".
+Another corner case is the path might contain multiple slashes '/' together, such as "/home//foo/".
+In this case, you should ignore redundant slashes and return "/home/foo".
+
+class Solution {
+    public String simplifyPath(String path) {
+        String[] paths = path.split("/", -1);
+        int j = 0;
+        Deque<String> deque = new LinkedList<>();
+        for(int i = 0; i < paths.length; ++i){
+            if(paths[i].equals("") || paths[i].equals("."))
+                continue;
+            if(paths[i].equals("..")){
+                if(!deque.isEmpty())
+                    deque.removeLast();
+            }
+            else{
+                deque.addLast(paths[i]);
+            }
+        }
+        if(deque.isEmpty())
+            return "/";
+        StringBuilder res = new StringBuilder();
+        while(!deque.isEmpty())
+            res.append("/" + deque.removeFirst());
+        return res.toString();
+    }
+}
