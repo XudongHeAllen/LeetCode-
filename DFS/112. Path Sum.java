cef7c2d92203ddd02f506a7531d9a5d558a7e09f
Xudong He <hex3456@gmail.com>
2018-12-10 17:13:56 -0600

    Create 112. Path Sum.java

---
+++ DFS/112. Path Sum.java
@@ -0,0 +1,34 @@
+Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given the below binary tree and sum = 22,
+
+      5
+     / \
+    4   8
+   /   / \
+  11  13  4
+ /  \      \
+7    2      1
+return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        return sum(root, sum);
+    }
+    private boolean sum(TreeNode root, int sum){
+        return root != null && (root.val == sum && root.left == null && root.right == null || sum(root.left, sum - root.val) || sum(root.right, sum - root.val));
+    }
+}
