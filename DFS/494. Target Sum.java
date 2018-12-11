8ede148a65812ce97041a85fb721f7b1a5d949ec
Xudong He <hex3456@gmail.com>
2018-12-11 00:28:43 -0600

    Update 494. Target Sum.java

--- DFS/494. Target Sum.java
+++ DFS/494. Target Sum.java
@@ -41,3 +41,21 @@ Note that the above formula has proved that target + sum(nums) must be even
 We can use that fact to quickly identify inputs that do not have a solution (Thanks to @BrunoDeNadaiSarnaglia for the suggestion)
 For detailed explanation on how to solve subset sum problem, you may refer to Partition Equal Subset Sum
 
+
+class Solution {
+    public int findTargetSumWays(int[] nums, int S) {
+        int sum = 0;
+        for(int n : nums)
+            sum += n;
+        return sum < S ||(S+sum) % 2 > 0 ? 0:subsetSum(nums, (S+sum)/2);
+    }
+    private int subsetSum(int[] nums, int s){
+        int[] dp = new int[s+1];
+        dp[0] = 1;
+        for(int n : nums){
+            for(int i = s; i >= n; i--)
+            dp[i] += dp[i-n];
+        }
+        return dp[s];
+    }
+}
